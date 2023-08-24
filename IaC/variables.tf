cd5b402cf8e9734061a8152582d544d7bb78686f
ThePreston <84995595+ThePreston@users.noreply.github.com>
2023-08-24 12:38:55 -0500

    Update variables.tf

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -8,47 +8,45 @@ variable "location" {
 variable "projectnamingconvention" {
   description = "Naming Convention"
   type        = string
-  default     = ""
+  default     = "env-gen"
 }
 
 variable "contactemail" {
   description = "email of the contact resource"
   type        = string
-  default     = ""
-}
-
-variable "subscriptionid" {
-  description = "subscription id of the contact resource"
-  type        = string
-  default     = ""
 }
 
 variable "companyname" {
   description = "company name of the contact resource"
   type        = string
-  default     = ""
 }
 
 variable "sqladminlogin" {
   description = "sql admin login"
   type        = string
-  default     = ""
 }
 
 variable "sqladminloginpassword" {
   description = "sql admin password"
   type        = string
-  default     = ""
 }
 
-variable "organization" {
-  description = "organization name"
+variable "subscriptionid" {
+  description = "subscription id of the contact resource"
   type        = string
-  default     = ""
 }
 
-variable "workspace" {
-  description = "workspace name"
+variable "client_id" {
+  description = "client id of the service principal"
   type        = string
-  default     = ""
+}
+
+variable "client_secret" {
+  description = "client secret of the service principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "tenant id of the service principal"
+  type        = string
 }
